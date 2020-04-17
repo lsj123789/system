9a5075c592e10f29c83ebd06ea5e3f02f80da914
今天刘思佳脸又圆了吗? <1375287603@qq.com>
2020-04-17 14:30:14 +0800

    add student system home component interfaces

--- Desktop/system_server/controller/company.js
+++ Desktop/system_server/controller/company.js
@@ -35,11 +35,79 @@ router.get("/getAllPublishInfo", async ctx => {
     ctx.body = res
   })
 })
+//删选通过审核的招聘信息
+router.get("/getPassPublishInfo", async ctx => {
+  const company = mongoose.model("Company")
+  const condition = ctx.request.query.condition
+  await company.find({ condition }).then(res => {
+    ctx.body = res
+  })
+})
+//通过学历筛选
+router.get("/getByEducational", async ctx => {
+  const company = mongoose.model("Company")
+  const educational = ctx.request.query.educational
+  await company.find({ "positionInfo.educational": educational }).then(res => {
+    ctx.body = res
+  })
+})
+
+//通过薪资筛选
+router.get("/getBySalary", async ctx => {
+  const company = mongoose.model("Company")
+  const pay = ctx.request.query.pay
+  await company.find({ "positionInfo.pay": pay }).then(res => {
+    ctx.body = res
+  })
+})
+
+//通过公司行业筛选
+router.get("/getByTrade", async ctx => {
+  const company = mongoose.model("Company")
+  const trade = ctx.request.query.trade
+  await company.find({ "companyInfo.trade": trade }).then(res => {
+    ctx.body = res
+  })
+})
+
+//通过公司规模筛选
+router.get("/getByScale", async ctx => {
+  const company = mongoose.model("Company")
+  const scale = ctx.request.query.scale
+  await company.find({ "companyInfo.scale": scale }).then(res => {
+    ctx.body = res
+  })
+})
+
+//通过融资阶段筛选
+router.get("/getByFinance", async ctx => {
+  const company = mongoose.model("Company")
+  const finance = ctx.request.query.finance
+  await company.find({ "companyInfo.finance": finance }).then(res => {
+    ctx.body = res
+  })
+})
+
+// 搜索
+router.get("/searchPositionInfo", async ctx => {
+  const company = mongoose.model("Company")
+  const { positionName, companyName } = ctx.request.query
+  await company
+    .find({
+      $or: [
+        { "companyInfo.companyName": companyName },
+        { "positionInfo.positionName": positionName }
+      ]
+    })
+    .then(res => {
+      ctx.body = res
+    })
+})
 
 router.post("/passPublishInfo", async ctx => {
   const company = mongoose.model("Company")
   const { id, condition } = ctx.request.body
-  await company.update({ _id: id }, { condition: condition }).then(res => {
+  await company.updateOne({ _id: id }, { condition: condition }).then(res => {
     ctx.body = {
       code: 200,
       message: "信息审核已通过"
